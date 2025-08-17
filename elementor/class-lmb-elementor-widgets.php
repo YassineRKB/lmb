f5b214b1fd784d262f1a76bc2017982fea529ef1
Yassine Rakibi <yassine@arcraven.com>
2025-08-17 15:46:06 +0100

    elementor widgets skeleton modification for near future widgets

--- elementor/class-lmb-elementor-widgets.php
+++ elementor/class-lmb-elementor-widgets.php
@@ -1,46 +1,31 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) {
-    exit;
-}
+if (!defined('ABSPATH')) exit;
 
-use Elementor\Widget_Base;
-
-class LMB_Elementor_Widgets {
-
-    public function __construct() {
-        add_action('elementor/widgets/register', array($this, 'register_widgets'));
-    }
-
-    public function register_widgets($widgets_manager) {
-        require_once LMB_CORE_PATH . 'elementor/widgets/class-lmb-ads-directory-widget.php';
-        require_once LMB_CORE_PATH . 'elementor/widgets/class-lmb-newspaper-directory-widget.php';
-        require_once LMB_CORE_PATH . 'elementor/widgets/class-lmb-invoice-widget.php';
-
-        $widgets_manager->register(new LMB_Ads_Directory_Widget());
-        $widgets_manager->register(new LMB_Newspaper_Directory_Widget());
-        $widgets_manager->register(new LMB_Invoice_Widget());
-    }
-}
-
-new LMB_Elementor_Widgets();
-
-class LMB_Elementor_Widgets_Helper {
-    public static function ads_directory_shortcode() {
+final class LMB_Elementor_Widgets_Helper {
+    public static function ads_directory_shortcode($atts=[]) {
         ob_start();
-        $widget = new LMB_Ads_Directory_Widget();
-        $widget->render();
+        the_widget('LMB_Ads_Directory_Widget');
         return ob_get_clean();
     }
-    public static function newspaper_directory_shortcode() {
+    public static function newspaper_directory_shortcode($atts=[]) {
         ob_start();
-        $widget = new LMB_Newspaper_Directory_Widget();
-        $widget->render();
-        return ob_get_clean();
-    }
-    public static function invoice_widget_shortcode() {
-        ob_start();
-        $widget = new LMB_Invoice_Widget();
-        $widget->render();
+        the_widget('LMB_Newspaper_Directory_Widget');
         return ob_get_clean();
     }
 }
+
+add_action('elementor/widgets/register', function($widgets_manager){
+    require_once __DIR__.'/widgets/class-lmb-ads-directory-widget.php';
+    require_once __DIR__.'/widgets/class-lmb-newspaper-directory-widget.php';
+    require_once __DIR__.'/widgets/class-lmb-subscribe-package-widget.php';
+    require_once __DIR__.'/widgets/class-lmb-upload-bank-proof-widget.php';
+    require_once __DIR__.'/widgets/class-lmb-admin-stats-widget.php';
+    require_once __DIR__.'/widgets/class-lmb-user-dashboard-widget.php';
+
+    $widgets_manager->register(new \LMB_Ads_Directory_Widget());
+    $widgets_manager->register(new \LMB_Newspaper_Directory_Widget());
+    $widgets_manager->register(new \LMB_Subscribe_Package_Widget());
+    $widgets_manager->register(new \LMB_Upload_Bank_Proof_Widget());
+    $widgets_manager->register(new \LMB_Admin_Stats_Widget());
+    $widgets_manager->register(new \LMB_User_Dashboard_Widget());
+});
