e50cef98dc11207c0df77a82c7a586ddd02bffdb
Yassine Rakibi <yassine@arcraven.com>
2025-08-19 19:30:14 +0100

    admin class update, fixing deprecated method and return array

--- includes/class-lmb-notification-manager.php
+++ includes/class-lmb-notification-manager.php
@@ -19,38 +19,6 @@ class LMB_Notification_Manager {
         return self::send_email(get_option('admin_email'), $subject, $message);
     }
     
-    public static function notify_ad_approved($user_id, $ad_id) {
-        $user = get_userdata($user_id);
-        if (!$user) return;
-        
-        $ad_title = get_the_title($ad_id);
-        $subject = sprintf(__('Your legal ad "%s" has been approved', 'lmb-core'), $ad_title);
-        $message = sprintf(__('Hello %s,<br><br>Your legal ad "%s" has been approved and published. You can view it and download the PDF from your dashboard.<br><br>Regards,<br>The LMB Team', 'lmb-core'), $user->display_name, $ad_title);
-        
-        self::send_email($user->user_email, $subject, $message);
-    }
-    
-    public static function notify_ad_denied($user_id, $ad_id, $reason) {
-        $user = get_userdata($user_id);
-        if (!$user) return;
-        
-        $ad_title = get_the_title($ad_id);
-        $subject = sprintf(__('Action required on your legal ad "%s"', 'lmb-core'), $ad_title);
-        $message = sprintf(__('Hello %s,<br><br>Your legal ad "%s" has been denied. Reason: %s<br><br>Please review the ad in your dashboard.<br><br>Regards,<br>The LMB Team', 'lmb-core'), $user->display_name, $ad_title, esc_html($reason));
-        
-        self::send_email($user->user_email, $subject, $message);
-    }
-    
-    public static function notify_payment_verified($user_id, $package_id, $points_added) {
-        $user = get_userdata($user_id);
-        if (!$user) return;
-        
-        $package_title = get_the_title($package_id);
-        $subject = __('Your account has been credited', 'lmb-core');
-        $message = sprintf(__('Hello %s,<br><br>Your payment for the "%s" package has been verified. %d points have been added to your account.<br><br>Regards,<br>The LMB Team', 'lmb-core'), $user->display_name, $package_title, $points_added);
-        
-        self::send_email($user->user_email, $subject, $message);
-    }
     
     // Add notification to user's notification list
     public static function add_user_notification($user_id, $title, $message, $type = 'info', $icon = 'fas fa-info-circle') {
