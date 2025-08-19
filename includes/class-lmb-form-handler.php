39ba518c9888861172e2ae35efbf4a5fba63563c
Yassine Rakibi <23704082+YassineRKB@users.noreply.github.com>
2025-08-19 15:24:57 +0100

    Updated class-lmb-form-handler.php

--- includes/class-lmb-form-handler.php
+++ includes/class-lmb-form-handler.php
@@ -111,6 +111,47 @@ class LMB_Form_Handler {
         return $post_id;
     }
     
+    /**
+     * Create legal ad from Elementor form data (alternative method)
+     */
+    public static function create_legal_ad_from_elementor($form_data, $full_text) {
+        $user_id = get_current_user_id();
+        if (!$user_id) {
+            throw new Exception(__('You must be logged in to submit an ad.', 'lmb-core'));
+        }
+
+        $ad_title = !empty($form_data['title']) ? sanitize_text_field($form_data['title']) : 
+                   (!empty($form_data['companyName']) ? sanitize_text_field($form_data['companyName']) : 
+                   sanitize_text_field($form_data['ad_type']) . ' - ' . wp_date('Y-m-d'));
+
+        $post_id = wp_insert_post([
+            'post_type'    => 'lmb_legal_ad',
+            'post_title'   => $ad_title,
+            'post_status'  => 'draft',
+            'post_author'  => $user_id,
+        ], true);
+
+        if (is_wp_error($post_id)) {
+            throw new Exception($post_id->get_error_message());
+        }
+
+        update_post_meta($post_id, 'ad_type', sanitize_text_field($form_data['ad_type']));
+        update_post_meta($post_id, 'full_text', wp_kses_post($full_text));
+        update_post_meta($post_id, 'lmb_status', 'draft');
+        update_post_meta($post_id, 'lmb_client_id', $user_id);
+        
+        // Store additional form data as meta
+        foreach ($form_data as $key => $value) {
+            if (!in_array($key, ['ad_type', 'title']) && !empty($value)) {
+                update_post_meta($post_id, 'form_' . $key, sanitize_text_field($value));
+            }
+        }
+        
+        self::log_activity('New legal ad #%d created via Elementor form by %s', $post_id, wp_get_current_user()->display_name);
+        
+        return $post_id;
+    }
+    
     private static function log_activity($msg, ...$args) {
         if (class_exists('LMB_Ad_Manager')) {
             LMB_Ad_Manager::log_activity(vsprintf($msg, $args));
