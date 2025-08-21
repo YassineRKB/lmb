2c2d01f930e170257a8f0f8b584b3f38994f694d
Yassine Rakibi <yassine@arcraven.com>
2025-08-21 20:07:45 +0100

    v2.2.0

--- lmb-core.php
+++ lmb-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LMB Core
  * Description: Elementor-first legal ads platform core (auth, CPTs, points, invoices, payments, PDFs, directories, dashboards).
- * Version: 2.1.11
+ * Version: 2.2.0
  * Author: Yassine Rakibi
  * Requires at least: 6.0
  * Requires PHP: 7.4
