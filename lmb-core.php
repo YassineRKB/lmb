5482c5e15ac5b68ec3cdd733a2a2e3d8c0708b30
Yassine Rakibi <yassine@arcraven.com>
2025-10-07 17:16:40 +0100

    final fix for balance manipulation and package admin force subscribe: start

--- lmb-core.php
+++ lmb-core.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: LMB Core
  * Description: Elementor-first legal ads platform core.
- * Version: 5.4.0
+ * Version: 5.5.0
  * Author: Yassine Rakibi
  * Requires at least: 6.0
  * Requires PHP: 7.4
